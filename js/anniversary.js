283a386174bf4d419b64a8cf4b5f19a63bc783e0
PicNic <offiksm0903@naver.com>
2021-10-04 00:02:05 +0900

    #7_2 Up to AnniversaryWidget
    completed js!!

--- js/anniversary.js
+++ js/anniversary.js
@@ -1,23 +1,58 @@
 const day = document.querySelector(".dDay span");
-const li = document.querySelectorAll(".dayList li");
+const ul = document.querySelector(".dayList");
+const dDaySpan = document.querySelector(".dDay span");
 
 const newYear = new Date(2022, 01, 01);
 const childrensDay = new Date(2022, 04, 05);
-const patentsDay = new Date(2022, 04, 05);
+const parentsDay = new Date(2022, 04, 08);
 const teachersDay = new Date(2022, 04, 15);
 const memorialDay = new Date(2022, 05, 06);
 const nfDayOfKorea = new Date(2022, 09, 03);
-const xMax = new Date(2021, 11, 25);
+const xMas = new Date(2021, 11, 25);
 
-function getDDay() {
+function getDDay(anniversary) {
+  let dDay = new Date();
+  dDay = anniversary;
   const date = new Date();
-  const remain = 변수.getTiem() - date.getTime();
+  const remain = dDay.getTime() - date.getTime();
+  const Day = Math.ceil(remain / (1000 * 60 * 60 * 24));
+  dDaySpan.innerText = `D - ${Day}`;
 }
 
 function checkDay(e) {
   e.preventDafault;
-  //const date;
-  console.log(e.target.className);
+  let anniversary = new Date();
+  if (e.target.className === "newYear") {
+    console.log(e.target.className);
+    anniversary = newYear;
+    getDDay(anniversary);
+  } else if (e.target.className === "childrensDay") {
+    console.log(e.target.className);
+    anniversary = childrensDay;
+    getDDay(anniversary);
+  } else if (e.target.className === "teachersDay") {
+    console.log(e.target.className);
+    anniversary = teachersDay;
+    getDDay(anniversary);
+  } else if (e.target.className === "parentsDay") {
+    console.log(e.target.className);
+    anniversary = parentsDay;
+    getDDay(anniversary);
+  } else if (e.target.className === "memorialDay") {
+    console.log(e.target.className);
+    anniversary = memorialDay;
+    getDDay(anniversary);
+  } else if (e.target.className === "nfDayOfKorea") {
+    console.log(e.target.className);
+    anniversary = nfDayOfKorea;
+    getDDay(anniversary);
+  } else if (e.target.className === "xMas") {
+    console.log(e.target.className);
+    anniversary = xMas;
+    getDDay(anniversary);
+  } else {
+    return;
+  }
 }
 
-li.addEventListener("click", checkDay);
+ul.addEventListener("click", checkDay);
